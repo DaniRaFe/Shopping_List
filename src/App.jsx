fa5661926fff40c6a96bc9bed2e20b5883d3474a
Daniela Ramon Feria <l19350318@tuxtepec.tecnm.mx>
2023-04-20 09:58:17 -0600

    lista de compras

---
+++ src/App.jsx
@@ -0,0 +1,100 @@
+import { useState } from "react"
+import ListItem from "./Components/ListItem";
+function App() {
+    const [listItems, setListItems] = useState([
+      {
+        id: "1",
+        name: "Arroz",
+        quantity: 1,
+        unit: "Kg",
+        checked: false,
+      },
+      {
+        id: "2",
+        name: "Frijol",
+        quantity: 1,
+        unit: "Kg",
+        checked: false,
+      },
+      {
+        id: "3",
+        name: "Leche",
+        quantity: 1,
+        unit: "Lts",
+        checked: false,
+      },
+      {
+        id: "4",
+        name: "Papel Higienico",
+        quantity: 1,
+        unit: "Pz",
+        checked: false,
+      }
+    ]);
+
+    const handleItemChecked = (e) => {
+      const newList = listItems.map(item => {
+        if (e.target.name === item.id) {
+          item.checked = !item.checked;
+        }
+        return item;
+      })
+
+      setListItems (newList);
+    }
+
+
+      // if (itemChecked[e.target.name]) {
+      //   setItemChecked({
+      //     ...itemChecked,
+      //     [e.target.name]: false,
+      //   });
+      // } else {
+      //   setItemChecked({
+      //     ...itemChecked,
+      //     [e.target.name]: true,
+      //   });
+      // }
+      //setListItems({
+      //  ...listItems,
+      //  [e.target.name]: !listItems[e.target.name]
+      //})
+    
+  return (
+  <div className="container text-center">
+      <div className="row">
+        <div className="col">
+          <h1>Shopping List</h1>
+          <hr />
+        </div>
+        <div className="col text-end">
+          <button type="button" className="btn btn-outline-primary">
+            <i className="bi bi-plus-circle"></i>
+          </button>
+        </div>
+      </div>
+      
+      <hr />
+      {
+        listItems.map((item) => (
+          <ListItem
+          id={item.id}
+          name={item.name}
+          quantity={item.quantity}
+          unit={item.unit}
+          checked={item.checked}
+          handleItemChecked={handleItemChecked}
+          />
+        ))
+      }
+     
+     
+
+    </div>
+  )
+}
+export default App
+
+
+
+
